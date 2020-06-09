4246c2eee0d25dc06c5b8606b5960a1406e7c7bd
TreeNewBeeing <zephre0101@gmail.com>
2020-06-09 09:53:08 -0700

    Fix: Pull Image Error.

--- pull-docker.sh
+++ pull-docker.sh
@@ -1,8 +1,10 @@
 . "$(dirname "$0")"/env.config;
 
-sudo docker tag hisgeomap-front-end registry.cn-beijing.aliyuncs.com/hisgeomap/hisgeomap-front-end:$HISGEOMAP_VERSION
 sudo docker pull registry.cn-beijing.aliyuncs.com/hisgeomap/hisgeomap-front-end:$HISGEOMAP_VERSION
-sudo docker tag hisgeomap-back-end registry.cn-beijing.aliyuncs.com/hisgeomap/hisgeomap-back-end:$HISGEOMAP_VERSION
+sudo docker tag registry.cn-beijing.aliyuncs.com/hisgeomap/hisgeomap-front-end:$HISGEOMAP_VERSION hisgeomap-front-end
+
 sudo docker pull registry.cn-beijing.aliyuncs.com/hisgeomap/hisgeomap-back-end:$HISGEOMAP_VERSION
-sudo docker tag hisgeomap-search registry.cn-beijing.aliyuncs.com/hisgeomap/hisgeomap-search:$HISGEOMAP_VERSION
+sudo docker tag registry.cn-beijing.aliyuncs.com/hisgeomap/hisgeomap-back-end:$HISGEOMAP_VERSION hisgeomap-back-end
+
 sudo docker pull registry.cn-beijing.aliyuncs.com/hisgeomap/hisgeomap-search:$HISGEOMAP_VERSION
+sudo docker tag registry.cn-beijing.aliyuncs.com/hisgeomap/hisgeomap-search:$HISGEOMAP_VERSION hisgeomap-search
